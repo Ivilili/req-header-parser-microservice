477a08cf7360a2b74087d05e617e365c73ac0ba9
Ivana Livaja <ivana.livaja@hotmail.com>
2018-11-16 17:03:25 +0100

    added style, whoami route

--- server.js
+++ server.js
@@ -1,20 +1,36 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var cors = require('cors');
+var useragent = require('express-useragent');
 
 var app = express();
 
 app.use(bodyParser.json());
 app.use(cors({ optionSuccessStatus: 200 }));
 app.use(express.static('public'));
+app.use(useragent.express());
 
 app.get('/', function(req, res) {
-	res.send('Hello World!');
+	console.log(req.url);
+	res.sendFile(__dirname + '/views/index.html');
+});
+
+app.get('/api/whoami', function(req, res, next) {
+	var header = req.headers;
+	var ip = req.headers.host;
+	var lang = req.headers['accept-language'];
+	var software = 'OS: ' + req.useragent.os + ', Browser: ' + req.useragent.browser;
+
+	res.json({
+		ipaddress: ip,
+		language: lang.slice(0, lang.indexOf(',')),
+		software: software
+	});
 });
 
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
-	console.log('Your app is listening on port ' + port);
+	console.log('Your app is listening on port' + port);
 });
 
 module.exports = app;
